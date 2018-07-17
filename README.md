48e66283d4572ea0a12de3f34cafbf71aa199f48
David Vilaça <david@xdevel.com.br>
2018-07-17 00:40:31 -0300

    feat(cli): first version of cli app

---
+++ README.md
@@ -0,0 +1,73 @@
+# Excel to JSON
+
+Transform excel file to JSON translating and/or mapping keys.
+
+## Usage
+
+```bash
+$ node src/index.js file.xls -d dict.json -m map_keys.json
+```
+
+- `file.xls` - Excel file
+- `dict.json` - Translate key file
+- `map_keys.json` - Map key file
+
+### File example
+
+Excel (excel.xlsx):
+
+| User Name | Age | Company | Role | id |
+| --------- | --- | ------- | ---- | --- |
+| Bill Gates | 62 | Microsoft | director | 1 |
+
+Dict (dict.json):
+```json
+{
+  "User Name": "user_name",
+  "Age": "age",
+  "Company": "company",
+  "Role": "role",
+  "id": "id"
+}
+```
+
+Map (map.json):
+```json
+{
+  "User Name": "user.name",
+  "Age": "user.age",
+  "Company": "user.company.name",
+  "Role": "user.company.role",
+  "id": "id"
+}
+```
+
+Runing: `$ node src/index.js excel.xlsx -d dict.json`
+Result:
+```json
+{
+  "user_name": "Bill Gates",
+  "age": "62",
+  "company": "Microsoft",
+  "role": "director",
+  "id": "1"
+}
+```
+
+
+
+Runing: `$ node src/index.js excel.xlsx -m map.json`
+Result:
+```json
+{
+  "user": {
+    "name": "Bill Gates",
+    "age": "62",
+    "company": {
+      "name": "Microsoft",
+      "role": "director"
+    }
+  },
+  "id": "1"
+}
+```
---
+++ src/index.js
@@ -0,0 +1,214 @@
+const path = require('path');
+const { writeFileSync, readFileSync, existsSync } = require('fs');
+const xlsx = require('node-xlsx');
+const Confirm = require('prompt-confirm');
+const commandLineArgs = require('command-line-args')
+const commandLineUsage = require('command-line-usage')
+
+/**
+ * CLI OPTIONS AND USAGE
+ */
+const options = commandLineArgs([
+  { name: 'createMap', alias: 'r', type: Boolean },
+  { name: 'createDict', alias: 'k', type: Boolean },
+  { name: 'help', alias: 'h', type: Boolean },
+  { name: 'file', alias: 'f', type: String, defaultOption: true },
+  { name: 'dictFile', alias: 'd', type: String },
+  { name: 'mapFile', alias: 'm', type: String },
+  { name: 'keysLine', alias: 'l', type: Number, defaultValue: 0 }
+])
+const usage = commandLineUsage([
+  {
+    header: 'EXCEL TO JSON APP',
+    content: 'Generate json file from excel file'
+  },
+  {
+    header: 'Options',
+    optionList: [
+      {
+        name: 'createMap',
+        typeLabel: '{underline -r}',
+        description: 'To create a json file to map the keys'
+      },
+      {
+        name: 'createDict',
+        typeLabel: '{underline -k}',
+        description: 'To create a json file to translate the keys'
+      },
+      {
+        name: 'file',
+        typeLabel: '{underline -f}',
+        description: 'Excel file'
+      },
+      {
+        name: 'dictFile',
+        typeLabel: '{underline -d}',
+        description: 'Json file with key dictionary.'
+      },
+      {
+        name: 'mapFile',
+        typeLabel: '{underline -m}',
+        description: 'Json file with key map.'
+      },
+      {
+        name: 'keysLine',
+        typeLabel: '{underline -l}',
+        description: 'Line number (excel) of keys.'
+      },
+      {
+        name: 'help',
+        typeLabel: '{underline -h}',
+        description: 'Show this usage guide.'
+      }
+    ]
+  },
+  {
+    content: '--file (-f) is default option'
+  }
+])
+
+
+/**
+ * Save a json file
+ *
+ * @param {string} filename
+ * @param {*} data
+ * @returns {void}
+ */
+function writeJsonFile(filename, data, encoding = 'utf-8', override = false, i = 0) {
+  if (i > 0) {
+    const _filename = filename.split('.')
+    _filename[0] = `${_filename[0]}-${i}`
+    filename = _filename.join('.')
+  }
+  if (!override && existsSync(filename)) {
+    const prompt = new Confirm(`O arquivo ${filename} já existe. Deseja sobrescreve-lo?`);
+    prompt.run()
+      .then(answer => answer ? writeJsonFile(filename, data, encoding, answer) : writeJsonFile(filename, data, encoding, answer, ++i));
+    return;
+  }
+  writeFileSync(path.join(process.cwd(), filename), JSON.stringify(data, null, 2), { encoding });
+  console.log(`Arquivo ${filename} salvo com sucesso em ${process.cwd()}.`);
+}
+
+/**
+ * Open json file and parse
+ *
+ * @param {string} filename
+ * @param {string} [encoding]
+ * @returns {*}
+ */
+function openJson(filename, encoding) {
+  try {
+    const file = readFileSync(path.join(process.cwd(), filename), { encoding } );
+    return JSON.parse(file);
+  } catch (e) {
+    throw new Error(`Não foi possível abrir o arquivo ${filename}`);
+  }
+}
+
+/**
+ * Excel file to json with keys translation
+ *
+ * @param {*} dataFile
+ * @param {{[key: string]: string}} excelKeys
+ * @param {{[key: string]: string}} [dict]
+ * @returns {*}
+ */
+function toJson(dataFile, excelKeys, dict) {
+  let _dataFile = [];
+  const translate = !!dict && typeof dict === 'object'
+  dataFile.forEach(line => {
+    let value = {}
+    line.forEach((col, i) => {
+      value[translate ? dict[excelKeys[i]] : excelKeys[i]] = col
+    });
+    _dataFile.push(value);
+  });
+  return _dataFile;
+}
+
+/**
+ * Map object keys format
+ *
+ * @param {any[]} arr
+ * @param {{[key: string]: string}} excelKeys
+ * @param {{[key: string]: string}} mapKeys
+ * @returns
+ */
+function mapObj(arr, excelKeys, mapKeys) {
+  return arr.map(oldValue => {
+    let newValue = {};
+    excelKeys.forEach(key => {
+      let mapedKeys = (mapKeys[key] || "").split('.');
+      if (mapedKeys.length === 1) {
+        newValue[mapedKeys[0]] = oldValue[key];
+        return;
+      }
+      const olds = [];
+      for (let i = 0; i < mapedKeys.length; i++) {
+        let isLast = mapedKeys.length - 1 === i;
+        let isFirst = i === 0;
+        if (isFirst) {
+          newValue[mapedKeys[i]] = newValue[mapedKeys[i]] || {};
+          olds.push(newValue[mapedKeys[i]]);
+        } else {
+          olds[olds.length - 1][mapedKeys[i]] = isLast ? oldValue[key] : olds[olds.length - 1][mapedKeys[i]] || {};
+          olds.push(olds[olds.length - 1][mapedKeys[i]])
+        }
+      }
+    })
+    return newValue;
+  });
+}
+
+/**
+ * Create json object for use in mapFile option
+ *
+ * @param {string[]} excelKeys
+ * @returns
+ */
+function createMap(excelKeys) {
+  let _toMapJson = {};
+  excelKeys.forEach(key => _toMapJson[key] = "");
+  return _toMapJson;
+}
+
+function main(options) {
+
+  const { file: xlsFileName, dictFile: dictFileName, mapFile: mapFileName, keysLine } = options
+
+  if (!options.file) {
+    console.log(usage)
+    return 1;
+  }
+
+  const file = xlsx.parse(path.join(process.cwd(), xlsFileName))[0];
+  const dictKeys = dictFileName ? openJson(dictFileName) : undefined;
+  const mapKeys = mapFileName ? openJson(mapFileName) : undefined;
+  const excelKeys = file.data[keysLine];
+  const data = file.data.slice(keysLine+1);
+  // Transform excel to JSON
+  let outputJson = toJson(data, excelKeys, dictKeys);
+  // Map keys
+  if (!!options.mapFile) {
+    outputJson = mapObj(outputJson, excelKeys, mapKeys)
+  }
+  // Save files
+  writeJsonFile('output.json', outputJson);
+  if (options.createMap) {
+    writeJsonFile('map_keys.json', createMap(excelKeys));
+  }
+  if (options.createDict) {
+    writeJsonFile('dict.json', createMap(excelKeys));
+  }
+  return 0;
+
+}
+
+if (options.help) {
+  console.log(usage);
+  process.exit(0);
+}
+
+main(options)
